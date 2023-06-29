8e8d51a65571ea5b66ec1e1bdb1eb0f739bd6ce4
KishoreSrinivasa <itsmekishore2003@gmail.com>
2023-06-29 16:30:33 +0530

    code updated

---
+++ index.js
@@ -0,0 +1,7 @@
+function add(){
+    var x = document.getElementById("num1").value; 
+    var y = document.getElementById("num2").value;
+    var z = parseInt(x) + parseInt(y);
+    document.getElementById("output1").value = z;
+}
+
